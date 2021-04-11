7f636a55f73af15338573b19f782325408433cf4
abid <chiheb.abid@gmail.com>
2021-04-11 01:41:07 +0100

    Adding function to control backlight

--- LCD.cpp
+++ LCD.cpp
@@ -2,10 +2,15 @@
 #include "pigpio.h"
 #include <cstdint>
 #include <unistd.h>
+#include <iostream>
 // Commands
-#define	LCD_CLEAR	0x01
-#define LCD_FUNCTIONSET 0x20
+#define    LCD_CLEAR    0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
 #define LCD_DISPLAYCONTROL 0x08
+#define LCD_FUNCTIONSET 0x20
+
+
 // flags for function set
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
@@ -23,90 +28,100 @@
 #define LCD_BLINKON 0x01
 #define LCD_BLINKOFF 0x00
 
-const uint8_t lcdRowOffset[] = { 0x80, 0xC0, 0x14, 0x54 };
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
 
-LCD::LCD(const uint8_t bus, uint8_t addr, uint8_t width, bool backlight_on, uint8_t RS, uint8_t RW, uint8_t E, uint8_t BL, uint8_t B4) {
-	m_i2cHandle = i2cOpen(bus, addr, 0);
-	m_backlight_on = backlight_on;
-	m_RS = 1 << RS;
-	m_E = 1 << E;
-	m_BL = 1 << BL;
-	m_B4 = B4;
-    m_displayFunction=LCD_FUNCTIONSET | LCD_2LINE | LCD_4BITMODE | LCD_5x8DOTS;
-    m_displayControl=LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;;
-	init();
+
+// flags for backlight control
+#define LCD_BACKLIGHT 0x08
+
+
+const uint8_t lcdRowOffset[] = {0x80, 0xC0, 0x14, 0x54};
+
+LCD::LCD(const uint8_t bus, uint8_t addr, uint8_t width, bool backlight_on, uint8_t RS, uint8_t RW, uint8_t E,
+         uint8_t B4) {
+    m_i2cHandle = i2cOpen(bus, addr, 0);
+    m_backlight_on = backlight_on;
+    m_RS = 1 << RS;
+    m_E = 1 << E;
+    m_B4 = B4;
+    m_displayFunction = LCD_FUNCTIONSET | LCD_2LINE | LCD_4BITMODE | LCD_5x8DOTS;
+    m_displayControl = LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;;
+    m_displayMode = LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+    init();
+    clear();
 }
 
 LCD::~LCD() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
+
 /*
  * Initialise the LCD screen
  */
 void LCD::init() const {
-	write4bits(0x3<<4);
-	usleep(4500);
-	write4bits(0x3<<4);
-	usleep(4500);
-	write4bits(0x3<<4);
-	usleep(150);
-	write4bits(0x2<<4);
-
-    sendCommand(m_displayFunction); // Function set
-    sendCommand(m_displayControl); //# Display on,move_to off, blink off
-	sendCommand(0x06); // Cursor Increment
-
-
-	clear(); //clear();
-	setPosition(0,0);
+    write4bits(0x3 << 4);
+    usleep(4500);
+    write4bits(0x3 << 4);
+    usleep(4500);
+    write4bits(0x3 << 4);
+    usleep(150);
+    write4bits(0x2 << 4);
+    sendCommand(m_displayFunction);
+    sendCommand(m_displayControl);
+    sendCommand(m_displayMode);
+    goHome();
 }
 
 void LCD::sendCommand(const uint8_t cmd) const {
-	uint8_t MSN = (cmd >> 4) & 0x0F;
-	uint8_t LSN = cmd & 0x0F;
-	uint8_t MSb = MSN << m_B4;
-	uint8_t LSb = LSN << m_B4;
-	sendByte(MSb, LSb);
+    uint8_t MSN = (cmd >> 4) & 0x0F;
+    uint8_t LSN = cmd & 0x0F;
+    uint8_t MSb = MSN << m_B4;
+    uint8_t LSb = LSN << m_B4;
+    sendByte(MSb, LSb);
 }
 
 void LCD::putData(const uint8_t bits) const {
-	uint8_t MSN = (bits >> 4) & 0x0F;
-	uint8_t LSN = bits & 0x0F;
-	uint8_t MSb = (MSN << m_B4) | m_RS;
-	uint8_t LSb = (LSN << m_B4) | m_RS;
-	sendByte(MSb, LSb);
+    uint8_t MSN = (bits >> 4) & 0x0F;
+    uint8_t LSN = bits & 0x0F;
+    uint8_t MSb = (MSN << m_B4) | m_RS;
+    uint8_t LSb = (LSN << m_B4) | m_RS;
+    sendByte(MSb, LSb);
 }
 
 void LCD::sendByte(uint8_t msb, uint8_t lsb) const {
-	if (m_backlight_on) {
-		lsb = lsb | m_BL;
-		msb = msb | m_BL;
-	}
-	write4bits(msb);
-	write4bits(lsb);
+    if (m_backlight_on) {
+        lsb = lsb | LCD_BACKLIGHT;
+        msb = msb | LCD_BACKLIGHT;
+    }
+    write4bits(msb);
+    write4bits(lsb);
 }
 
 void LCD::write4bits(uint8_t value) const {
-	i2cWriteByte(m_i2cHandle, value | m_E);
-	usleep(1);
-	i2cWriteByte(m_i2cHandle, value & ~m_E);
-	usleep(50);
+    i2cWriteByte(m_i2cHandle, value | m_E);
+    usleep(1);
+    i2cWriteByte(m_i2cHandle, value & ~m_E);
+    usleep(50);
 }
 
 /*
  * Move the cursor to a position
  */
 void LCD::setPosition(const uint8_t x, const uint8_t y) const {
-	sendCommand(lcdRowOffset[y] + x);
-	usleep(2000);
+    sendCommand(lcdRowOffset[y] + x);
+    usleep(2000);
 }
 
 /*
  * Clear the LCD screen
  */
 void LCD::clear() const {
-	sendCommand(LCD_CLEAR);
-	usleep(2000);
+    sendCommand(LCD_CLEAR);
+    usleep(2000);
 
 }
 
@@ -114,6 +129,22 @@ void LCD::clear() const {
  * Write a string
  */
 void LCD::puts(const char *str) {
-	while (*str)
-		putData((uint8_t) *(str++));
+    while (*str)
+        putData((uint8_t) *(str++));
+}
+
+/*
+ * Set the cursor to position 0,0
+ */
+void LCD::goHome() const {
+    sendCommand(LCD_RETURNHOME);
+    usleep(2000);
+}
+
+/*
+ * Enable/Disable backlight
+ */
+void LCD::enableBacklight(bool backlight_on) {
+    m_backlight_on=backlight_on;
+    i2cWriteByte(m_i2cHandle, m_backlight_on ? LCD_BACKLIGHT : 0);
 }
--- LCD.h
+++ LCD.h
@@ -9,18 +9,21 @@ private:
 	uint8_t m_RS,m_E,m_BL,m_B4;
 	uint8_t m_displayFunction; // Mode 4/8bits ; #lines ; font
     uint8_t m_displayControl;
+    uint8_t m_displayMode;
 	void init() const;
 	void write4bits(uint8_t value) const;
+    void sendCommand(const uint8_t cmd) const;
+    void sendByte(uint8_t lsb, uint8_t msb) const;
 public:
-	LCD(const uint8_t bus, uint8_t addr, uint8_t width = 16, bool backlight_on = true, uint8_t RS = 0, uint8_t RW = 1, uint8_t E = 2, uint8_t BL = 3,
+	LCD(const uint8_t bus, uint8_t addr, uint8_t width = 16, bool backlight_on = true, uint8_t RS = 0, uint8_t RW = 1, uint8_t E = 2,
 			uint8_t B4 = 4);
 	virtual ~LCD();
-	void sendCommand(const uint8_t cmd) const;
-	void sendByte(uint8_t lsb, uint8_t msb) const;
 	void setPosition(const uint8_t x, const uint8_t y) const;
 	void putData(const uint8_t bits) const;
 	void puts (const char *str);
 	void clear() const;
+	void goHome() const;
+    void enableBacklight(bool backlight_on=true);
 };
 
 #endif /* LCD_H_ */
